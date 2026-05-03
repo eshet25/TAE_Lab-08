79759462e7f39e1bdd19d1502bc7bbd147bbae4a
TsionAEsh <eshet25@wfu.edu>
2026-05-02 22:27:16 -0400

    script 03 done and saved new dataset

--- scripts/03-scrape-page-many.R
+++ scripts/03-scrape-page-many.R
@@ -7,14 +7,19 @@ library(glue)
 # list of urls to be scraped ---------------------------------------------------
 
 root <- "https://collections.ed.ac.uk/art/search/*:*/Collection:%22edinburgh+college+of+art%7C%7C%7CEdinburgh+College+of+Art%22?offset="
-numbers <- seq(from = ___, to = ___, by = ___)
-urls <- glue("{___}{___}")
+#I am scraping the first 220 records
+numbers <- seq(from = 0, to = 210, by = 10)
+urls <- glue("{root}{numbers}")
 
 # map over all urls and output a data frame ------------------------------------
 
-#🛑 this takes several minutes to run 🛑 
-___ <- map_dfr(___, ___)
+#🛑 this takes several minutes to run 🛑
+uoe_art <- map_dfr(urls, scrape_page)
+
 
 # write out data frame ---------------------------------------------------------
 
 write_csv(uoe_art, file = "data/uoe-art.csv")
+
+#checking
+glimpse(uoe_art)
